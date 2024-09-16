4d0c5f2a164c101c3a522eced3330e9425dff705
Rahman123L <rizkyrambe98@gmail.com>
2024-09-16 14:32:25 +0700

    first commit

--- js/subPoint.js
+++ js/subPoint.js
@@ -1,16 +1,31 @@
 function app() {
     return {
-        submenus: [],
-        addSubmenu(parentSubmenu = null) {
+        menus: [],
+        showButtons: false,
+        addmenu() {
+            this.menus.push({
+                title: '',
+                submenus: []
+            });
+            this.updateShowButtons();
+        },
+        addSubmenu(parentMenu) {
             const newSubmenu = {
                 title: '',
                 submenus: []
             };
-            if (parentSubmenu) {
-                parentSubmenu.submenus.push(newSubmenu);
-            } else {
-                this.submenus.push(newSubmenu);
-            }
-        }
+            parentMenu.submenus.push(newSubmenu);
+        },
+        hapus(index) {
+            this.menus.splice(index, 1);
+            this.updateShowButtons();
+        },
+        hapusAll() {
+            this.menus = [];
+            this.updateShowButtons();
+        },
+        updateShowButtons() {
+            this.showButtons = this.menus.length > 0;
+        },
     }
 }
